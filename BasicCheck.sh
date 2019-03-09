787e6b799e694d976a07b14cbf6ec01747aa242a
DennisG1993 <dennis.g1993@gmail.com>
2019-03-09 20:19:03 +0200

    Final Exc 1

---
+++ BasicCheck.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#this script sends stdout and stderr to /dev/null. im doing it since the requested output didnt include those.
+dirpath=$1
+progname=$2
+
+cd $dirpath
+
+make 2>/dev/null 1>/dev/null
+makesucc=$?
+
+if [ $makesucc -ne 0 ]
+then
+  echo "Compilation  Memory leaks  Thread race"
+  echo "   FAIL          FAIL          FAIL   "
+  exit 7 #since make failed it means that val and hel also failed hence the number 111 in binary hence 7 in decimal.
+else
+  #i can use those both valgrinds with ./$progname and it will work because of cd $dirpath command beforehand but better safe than sorry hence the $dirpath/$progname
+  valgrind --error-exitcode=2 --leak-check=full --show-leak-kinds=all  $dirpath/$progname 2>/dev/null 1>/dev/null  #im using full leak check and show all kinds so that i wouldn't miss any leak.
+  valsucc=$?
+
+  valgrind --tool=helgrind --error-exitcode=1  $dirpath/$progname 2>/dev/null 1>/dev/null
+  helsucc=$?
+
+  exitcode=$((helsucc+valsucc)) #i specifically set error exit code on val and hel to 2 and 1 so that me exit codes will be the same as the exercises demands.
+
+
+case "$exitcode" in
+  "0")
+    echo "Compilation  Memory leaks  Thread race"
+    echo "   PASS          PASS          PASS   "
+    exit $exitcode
+    ;;
+  "1")
+    echo "Compilation  Memory leaks  Thread race"
+    echo "   PASS          PASS          FAIL   "
+    exit $exitcode
+    ;;
+  "2")
+    echo "Compilation  Memory leaks  Thread race"
+    echo "   PASS          FAIL          PASS   "
+    exit $exitcode
+    ;;
+  "3")
+    echo "Compilation  Memory leaks  Thread race"
+    echo "   PASS          FAIL          FAIL   "
+    exit $exitcode
+    ;;
+  esac
+fi
