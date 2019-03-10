4f82cd87977d1cce37bb8fee32447cc5222013a1
DennisG1993 <dennis.g1993@gmail.com>
2019-03-10 16:52:28 +0200

    Final BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -2,7 +2,7 @@
 #this script sends stdout and stderr to /dev/null. im doing it since the requested output didnt include those.
 dirpath=$1
 progname=$2
-
+shift 2 #it means that $3 is $1 now and so on, this way i dont need to cut arguments later on.
 cd $dirpath
 
 make 2>/dev/null 1>/dev/null
@@ -12,13 +12,14 @@ if [ $makesucc -ne 0 ]
 then
   echo "Compilation  Memory leaks  Thread race"
   echo "   FAIL          FAIL          FAIL   "
+  cd - 1>/dev/null
   exit 7 #since make failed it means that val and hel also failed hence the number 111 in binary hence 7 in decimal.
 else
   #i can use those both valgrinds with ./$progname and it will work because of cd $dirpath command beforehand but better safe than sorry hence the $dirpath/$progname
-  valgrind --error-exitcode=2 --leak-check=full --show-leak-kinds=all  $dirpath/$progname 2>/dev/null 1>/dev/null  #im using full leak check and show all kinds so that i wouldn't miss any leak.
+  valgrind --error-exitcode=2 --leak-check=full --show-leak-kinds=all  $dirpath/$progname $@ 2>/dev/null 1>/dev/null  #im using full leak check and show all kinds so that i wouldn't miss any leak.
   valsucc=$?
 
-  valgrind --tool=helgrind --error-exitcode=1  $dirpath/$progname 2>/dev/null 1>/dev/null
+  valgrind --tool=helgrind --error-exitcode=1  $dirpath/$progname $@ 2>/dev/null 1>/dev/null
   helsucc=$?
 
   exitcode=$((helsucc+valsucc)) #i specifically set error exit code on val and hel to 2 and 1 so that me exit codes will be the same as the exercises demands.
@@ -28,21 +29,25 @@ case "$exitcode" in
   "0")
     echo "Compilation  Memory leaks  Thread race"
     echo "   PASS          PASS          PASS   "
+    cd - 1>/dev/null
     exit $exitcode
     ;;
   "1")
     echo "Compilation  Memory leaks  Thread race"
     echo "   PASS          PASS          FAIL   "
+    cd - 1>/dev/null
     exit $exitcode
     ;;
   "2")
     echo "Compilation  Memory leaks  Thread race"
     echo "   PASS          FAIL          PASS   "
+    cd - 1>/dev/null
     exit $exitcode
     ;;
   "3")
     echo "Compilation  Memory leaks  Thread race"
     echo "   PASS          FAIL          FAIL   "
+    cd - 1>/dev/null
     exit $exitcode
     ;;
   esac
